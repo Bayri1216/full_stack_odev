c9d9dc9835214892fbf8153fc0f032b87d8ffb92
bayri <bayriseyfullah3@gmail.com>
2023-12-28 19:41:33 +0300

    kutupane

--- controllers/books.js
+++ controllers/books.js
@@ -1,8 +1,56 @@
-const Book = require('../models/Book')
+const { query } = require('express');
+const Book = require('../models/Book');
+const Joi = require('joi');
+
+// Kitap nesnesi doğrulama şeması
+const kitapSchema = Joi.object({
+    barkod: Joi.string().max(20).required(),
+    yayinevi: Joi.string().required(),
+    stok: Joi.number().integer().min(0).required(),
+    fiyat: Joi.number().min(0).required(),
+});
+
+// Kitap ekleme endpoint'i
+app.post('/ekle', (req, res) => {
+    const { error } = kitapSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    // Verileri kullanarak kitap nesnesi oluşturabilirsiniz
+    const yeniKitap = {
+        barkod: req.body.barkod,
+        yayinevi: req.body.yayinevi,
+        stok: req.body.stok,
+        fiyat: req.body.fiyat,
+    };
+
+    // Burada kitap nesnesini kullanarak istediğiniz işlemleri yapabilirsiniz (örneğin, veritabanına ekleme)
+
+    res.json({ success: true, message: 'Kitap başarıyla eklendi.', kitap: yeniKitap });
+});
+
+// Ana sayfa
+app.get('/', (req, res) => {
+    res.send('Havalı Kitap Projesine Hoş Geldiniz!');
+});
+
 
 const getAllBooks = async (req, res) => {
 
-    const books = await Book.find({});
+ //skip - atla demek
+    //limit - şu kadar göster demek
+    //query ile page ve limit alıp skip bunlarla hesaplatıyor
+
+const page = number(req.query.page) || 1
+const limit = number(req.query.limit) || 10
+const skip = (page -1) * limit
+
+console.log({sayfa: page, limit: limit, atla: skip});
+
+
+    const books = await Book.find({}).skip(skip).limit(limit);
     return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
 
 }
